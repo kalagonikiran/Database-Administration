e30ec7b2381448cc3bb5303a4b363f9b7aca88d2
Kiran <83135224+kalagonikiran@users.noreply.github.com>
2024-02-16 18:13:19 +0530

    Create improving_performance.sql

---
+++ improving_performance.sql
@@ -0,0 +1,9 @@
+/*To download the zip file containing the database, copy and paste the following into the Terminal:
+wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0231EN-SkillsNetwork/datasets/employeesdb.zip
+unzip employeesdb.zip
+cd employeesdb
+mysql --host=127.0.0.1 --port=3306 --user=root --password -t < employees.sql 
+when prompted enter the password
+*/
+/*inside the mysql cli*/
+use employees
